1d44280c6130b8c47eee1dc361c873437ab22f27
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-09 23:58:46 +0545

    api call

---
+++ html/dadjokes/script.js
@@ -0,0 +1,14 @@
+// promises
+
+// 2cond => sucss , fail
+
+// fetch(`https://restcountries.com/v3.1/name/nepal`);
+// fullfilled;
+// reject;
+
+// fetch("https://restcountries.com/v3.1/name/nepal");
+// GET https://icanhazdadjoke.com/
+// async;
+
+const jokes = document.querySelector(`jokes`);
+const jokeBtn = document.querySelector(`#jokebtn`);
