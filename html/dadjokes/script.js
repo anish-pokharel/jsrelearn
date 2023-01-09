c94acdf42b027c8c2fc80919299caba435f63bcf
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-10 01:35:23 +0545

    json await async

--- html/dadjokes/script.js
+++ html/dadjokes/script.js
@@ -13,21 +13,48 @@
 const jokes = document.querySelector("joke");
 const jokeBtn = document.querySelector(`#jokeBtn`);
 
-const generateJokes = () => {
-  const setHeader = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-  fetch(`https://icanhazdadjoke.com`, setHeader)
-    .then((res) => res.json())
-    .then((data) => {
-      joke.innerHTML = data.joke;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+// const generateJokes = () => {
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch(`https://icanhazdadjoke.com`, setHeader)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       joke.innerHTML = data.joke;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+// jokeBtn.addEventListener("click", generateJokes);
+// generateJokes();
+
+// async function generateJokes(){}
+
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch(`https://icanhazdadjoke.com`, setHeader);
+    const data = await res.json();
+    joke.innerHTML = data.joke;
+  } catch (err) {
+    console.log(`the error is ${err}`);
+  }
 };
+// .then((res) => res.json())
+// .then((data) => {
+//   joke.innerHTML = data.joke;
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
 
 jokeBtn.addEventListener("click", generateJokes);
 generateJokes();
