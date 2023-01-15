3c917554117c9b74a741419c03090c7f8cb8ddf5
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-15 22:58:48 +0545

    6th push

--- incrementmethod/script.js
+++ incrementmethod/script.js
@@ -9,29 +9,4 @@ counters.forEach((counter) => {
     console.log(typeof targetCounter);
   };
   updateCounter();
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus id
-    voluptatem placeat ad distinctio ratione numquam deleniti recusandae ipsa.
-    Ipsum ex ab reiciendis? Quia maxime eum aut veniam. Reprehenderit!
-  </p>;
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus id
-    voluptatem placeat ad distinctio ratione numquam deleniti recusandae ipsa.
-    Ipsum ex ab reiciendis? Quia maxime eum aut veniam. Reprehenderit!
-  </p>;
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus id
-    voluptatem placeat ad distinctio ratione numquam deleniti recusandae ipsa.
-    Ipsum ex ab reiciendis? Quia maxime eum aut veniam. Reprehenderit!
-  </p>;
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus id
-    voluptatem placeat ad distinctio ratione numquam deleniti recusandae ipsa.
-    Ipsum ex ab reiciendis? Quia maxime eum aut veniam. Reprehenderit!
-  </p>;
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus id
-    voluptatem placeat ad distinctio ratione numquam deleniti recusandae ipsa.
-    Ipsum ex ab reiciendis? Quia maxime eum aut veniam. Reprehenderit!
-  </p>;
 });
